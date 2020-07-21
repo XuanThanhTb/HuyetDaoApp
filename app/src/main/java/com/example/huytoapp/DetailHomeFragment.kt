9b1f5c844b183fd925f15aa6e1980cf9386f6f1f
XuanThanhTb <xuanthanh180599@gmail.com>
2020-07-21 10:58:18 +0700

    chieu oi la chieu .chieu keo code ve nha mjk fix xong anh cho p roi do . dm

--- app/src/main/java/com/example/huytoapp/DetailHomeFragment.kt
+++ app/src/main/java/com/example/huytoapp/DetailHomeFragment.kt
@@ -4,213 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import androidx.recyclerview.widget.RecyclerView
 import com.example.huytoapp.ui.ThongTinBenhNhan.BaseFragment
-import kotlinx.android.synthetic.main.cell_detail_home_anh_mot.view.*
 
 
 class DetailHomeFragment : BaseFragment() {
-    var listAnh = arrayListOf<Int>(1, 2, 3, 5, 6, 7, 8, 9)
 
-    var listDataHuyet: ArrayList<DataHuyet> = arrayListOf(
-        DataHuyet(id = 1, color = "#FFEB3B", nameHuyet = "Thừa Khấp"),
-        DataHuyet(id = 2, color = "#FFEB3B", nameHuyet = "Thừa Khấp"),
-        DataHuyet(id = 1, color = "#89F809", nameHuyet = "Nhân trung"),
-        DataHuyet(id = 2, color = "#89F809", nameHuyet = "Phủ đột")
-    )
-
-    var touchData: String? = null
-
-    class DetailHomeFragment : Fragment() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-            arguments?.let {
-//            touchData = it?.getString("detailHome")
-            }
-//        val callback = requireActivity().onBackPressedDispatcher.addCallback(this) {
-//
-//        }
-            val callback: OnBackPressedCallback =
-                object : OnBackPressedCallback(true /* enabled by default */) {
-                    override fun handleOnBackPressed() {
-                        //
-                    }
-                }
-            requireActivity().onBackPressedDispatcher.addCallback(this, callback)
-        }
-
-        override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            return inflater.inflate(R.layout.fragment_detail_home, container, false)
-        }
-
-
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-//        setUpRecyclerViewDetailHome()
-//        toolbar_home_title.text = touchData ?: "Chi tiết châm cứu"
-
-
-        }
-
-//    private fun setUpRecyclerViewDetailHome() {
-//        recyDetailHome.layoutManager = LinearLayoutManager(context)
-//        recyDetailHome.adapter = AdapterDetailHome(listAnh, activity as MainActivity)
-//    }
-
-        companion object {
-            @JvmStatic
-            fun newInstance() = DetailHomeFragment()
-        }
-
-        inner class AdapterDetailHome(
-            var listAnh: java.util.ArrayList<Int>,
-            var activity: MainActivity
-        ) :
-            RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-
-            override fun onCreateViewHolder(
-                parent: ViewGroup,
-                viewType: Int
-            ): RecyclerView.ViewHolder {
-                if (viewType == 1) {
-                    var itemView = LayoutInflater.from(parent.context)
-                        .inflate(R.layout.cell_detail_home_anh_mot, parent, false)
-                    return ViewHolderMot(itemView)
-                } else if (viewType == 2) {
-                    var itemView = LayoutInflater.from(parent.context)
-                        .inflate(R.layout.cell_detail_home_anh_mot, parent, false)
-                    return ViewHolderHai(itemView)
-                } else if (viewType == 3) {
-                    var itemView = LayoutInflater.from(parent.context)
-                        .inflate(R.layout.cell_detail_home_anh_ba, parent, false)
-                    return ViewHolderBa(itemView)
-//        }else if (viewType == 4){
-//            var itemView = LayoutInflater.from(parent.context).inflate( R.layout.cell_detail_home_anh_bon, parent,false)
-//            return ViewHolderBon(itemView)
-                } else if (viewType == 5) {
-                    var itemView = LayoutInflater.from(parent.context)
-                        .inflate(R.layout.cell_detail_home_anh_nam, parent, false)
-                    return ViewHolderNam(itemView)
-                } else if (viewType == 6) {
-                    var itemView = LayoutInflater.from(parent.context)
-                        .inflate(R.layout.cell_detail_home_anh_sau, parent, false)
-                    return ViewHolderSau(itemView)
-                }
-//            else if (viewType == 7) {
-//                var itemView = LayoutInflater.from(parent.context)
-//                    .inflate(R.layout.cell_detail_home_anh_bay, parent, false)
-//                return ViewHolderBay(itemView)
-//            }
-                else if (viewType == 8) {
-                    var itemView = LayoutInflater.from(parent.context)
-                        .inflate(R.layout.cell_detail_home_anh_tam, parent, false)
-                    return ViewHolderTam(itemView)
-                } else return ViewHolderChin(
-                    LayoutInflater.from(parent.context)
-                        .inflate(R.layout.cell_detail_home_anh_chin, parent, false)
-                )
-
-            }
-
-            override fun getItemCount(): Int = listAnh.size
-            override fun getItemViewType(position: Int): Int {
-                return if (listAnh[position] == 1) 1
-                else if (listAnh[position] == 2) 2
-                else if (listAnh[position] == 3) 3
-//        else if (listAnh[position] == 4) 4
-                else if (listAnh[position] == 5) 5
-                else if (listAnh[position] == 6) 6
-//            else if (listAnh[position] == 7) 7
-                else if (listAnh[position] == 8) 8
-                else 9
-            }
-
-            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-                if (listAnh[position] == 1) {
-                    onPushDataHuyet(
-                        idButton = holder.itemView.buttonDenHuyet1,
-                        dataHuyetDao = "Trung Phủ"
-                    )
-                }
-                if (listAnh[position] == 2) {
-
-                }
-                if (listAnh[position] == 3) {
-
-                }
-//            if (listAnh[position] == 4) {
-//
-//            }
-                if (listAnh[position] == 5) {
-
-                }
-                if (listAnh[position] == 6) {
-
-                }
-//            if (listAnh[position] == 7) {
-//
-//            }
-                if (listAnh[position] == 8) {
-
-                }
-                if (listAnh[position] == 9) {
-
-                }
-            }
-
-            fun onPushDataHuyet(dataHuyetDao: String?, idButton: Button?) {
-                idButton?.setOnClickListener {
-                    Toast.makeText(activity, "action", Toast.LENGTH_LONG).show()
-//                launch {
-//                    context?.let {
-//                        if (dataHuyetDao != null) {
-//                            val mListButtonHuyet = ButtonHuyet(tenHuyet = dataHuyetDao)
-//                            it.toast("Đã thêm dữ liệu!")
-//                            ButtonHuyetDataBase(it).getButtonHuyetDao()
-//                                .addButtonHuyet(mListButtonHuyet)
-//                            val action =
-//                                DetailHomeFragmentDirections.actionDetailHomeFragmentToPhacDoHuyetFragment() //action chuyển tới màn nào
-//                            Navigation.findNavController(view).navigate(action)
-//                        }
-//
-                    var bundleDataHuyet = Bundle().apply {
-                        putString("phacdo", dataHuyetDao ?: "data")
-                    }
-                    Toast.makeText(activity, "ACTION", Toast.LENGTH_LONG).show()
-                    Navigation.findNavController(it)
-                        .navigate(R.id.phacDoHuyetFragment, bundleDataHuyet)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+//            val callback: OnBackPressedCallback =
+//                object : OnBackPressedCallback(true /* enabled by default */) {
+//                    override fun handleOnBackPressed() {
+//                        //
 //                    }
 //                }
-                }
-            }
-        }
+//            requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
 
-        inner class ViewHolderMot(itemView: View) : RecyclerView.ViewHolder(itemView)
-        inner class ViewHolderHai(itemView: View) : RecyclerView.ViewHolder(itemView)
-        inner class ViewHolderBa(itemView: View) : RecyclerView.ViewHolder(itemView)
-        inner class ViewHolderBon(itemView: View) : RecyclerView.ViewHolder(itemView)
-        inner class ViewHolderNam(itemView: View) : RecyclerView.ViewHolder(itemView)
-        inner class ViewHolderSau(itemView: View) : RecyclerView.ViewHolder(itemView)
-        inner class ViewHolderBay(itemView: View) : RecyclerView.ViewHolder(itemView)
-        inner class ViewHolderTam(itemView: View) : RecyclerView.ViewHolder(itemView)
-        inner class ViewHolderChin(itemView: View) : RecyclerView.ViewHolder(itemView)
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail_home, container, false)
     }
 }
 
-class DataHuyet(var id: Int?, var color: String?, var nameHuyet: String?)
 
 
 
